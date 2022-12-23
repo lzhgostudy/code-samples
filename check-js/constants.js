7d2131e50e1c0470ae21016b903bca1a75bcef58
Zhihua.Lu <luzhihuaforhankle@gmail.com>
2022-12-23 10:11:57 +0800

    checkJS

---
+++ check-js/constants.js
@@ -0,0 +1,5 @@
+/**
+ * checkJs = true, 报错
+ * 根据 TypeScript 自带的 parseFloat 类型定义，这是不正确的 JavaScrip
+ */
+module.exports.pi = parseFloat(3.124); // parseFloat 仅接受一个字符串作为参数
---
+++ check-js/main.ts
@@ -0,0 +1,11 @@
+/**
+ * @description
+ * checkJS
+ * 
+ * 与 allowJs 配合使用，当 checkJs 被启用时，JavaScript 文件中会报告错误。
+ * 也就是相当于在项目中所有 JavaScript 文件顶部包含 // @ts-check。
+ */
+
+import { pi } from "./constants";
+
+console.log(pi);
