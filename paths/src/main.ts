9d63c46a1d20ea1127ffe32efd2d35c06e319d4c
Zhihua.Lu <luzhihuaforhankle@gmail.com>
2022-12-23 20:40:43 +0800

    paths

---
+++ paths/src/main.ts
@@ -0,0 +1,11 @@
+/**
+ * @description
+ * paths
+ * 
+ * 一些将模块导入重新映射到相对于 baseUrl 路径的配置
+ * paths 可以允许你声明 TypeScript 应该如何解析你的 require/import。
+ */
+
+import { PI } from "@utils/const";
+
+console.log(PI);
