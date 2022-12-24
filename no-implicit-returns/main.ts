5db9c8dd81758fce156345beb3888e44702153ce
Zhihua.Lu <luzhihuaforhankle@gmail.com>
2022-12-24 10:34:08 +0800

    noImplicitReturns

---
+++ no-implicit-returns/main.ts
@@ -0,0 +1,14 @@
+/**
+ * @description
+ * noImplicitReturns
+ * 
+ * 是否禁止隐式的函数返回值
+ */
+
+function lookupHeadphonesManufacturer(color: "blue" | "black") {
+  if (color === "blue") {
+    return "beats";
+  } else {
+    "bose";
+  }
+}
