0678048955d664cc81632e2542e6bd486d3ed8c3
Zhihua.Lu <luzhihuaforhankle@gmail.com>
2022-12-24 15:54:13 +0800

    noImplicitThis

---
+++ no-implicit-this/main.ts
@@ -0,0 +1,24 @@
+/**
+ * @description
+ * noImplicitThis
+ * 
+ * 是否禁止隐式的 this
+ */
+
+class Rectangle {
+  width: number;
+  height: number;
+ 
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+ 
+  getAreaFunction() {
+    return function () {
+      return this.width * this.height;
+// 'this' implicitly has type 'any' because it does not have a type annotation.
+// 'this' implicitly has type 'any' because it does not have a type annotation.
+    };
+  }
+}
